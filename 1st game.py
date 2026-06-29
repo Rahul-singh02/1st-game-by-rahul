278383c66fec5a4955737832f12071c404b8dff4
RahulCooder <bb967209@gmail.com>
2026-06-29 23:15:25 +0530

    added snake lenth increse mechnism

--- 1st game.py
+++ 1st game.py
@@ -8,7 +8,7 @@ window.center_window()
 start_color = (255,255,255,95)
 class Snake:
     def __init__(self):
-    
+        self.body = [[523,124]]
         self.center_x = 675
         self.center_y = 343
         self.width = 30
@@ -20,15 +20,15 @@ class Snake:
         self.speed = 300
         
     def draw(self):
-    
-        square = arcade.draw_rectangle_filled(
-        self.center_x,
-        self.center_y, 
-        self.width, 
-        self.height,
-        color = self.color,
-        tilt_angle = self.angle,
-        )
+        for body in self.body:
+            arcade.draw_rectangle_filled(
+            body[0],
+            body[1],
+            self.width,
+            self.height,
+            color = self.color,
+            tilt_angle = self.angle,
+            )
 class Food:
     def __init__(self):
         self.app_x = random.random() * 1350
@@ -85,24 +85,24 @@ class GameView(arcade.View):
     
     def on_update(self, delta_time:float):
         if not self.game_over:
-            self.snake.center_x += self.snake.speed_x*delta_time
-            self.snake.center_y += self.snake.speed_y*delta_time
-        
-            if self.snake.center_x >= 1350 - self.snake.width/2:
-                self.game_over = True
-        
-            elif self.snake.center_x <= self.snake.width/2:
-                self.game_over = True
-        
-            elif self.snake.center_y >= 687 - self.snake.height/2:
-                self.game_over = True
-        
-            elif self.snake.center_y <= self.snake.height/2:
-                self.game_over = True
+            currnt_head = self.snake.body[0]
+            new_head_x = currnt_head[0] + self.snake.speed_x*delta_time
+            new_head_y = currnt_head[1] + self.snake.speed_y*delta_time
             
-            dis = arcade.get_distance(self.food.app_x, self.food.app_y, self.snake.center_x, self.snake.center_y)
+            self.snake.body.insert(0, [new_head_x, new_head_y])
+            
+            if new_head_x >= 1350 - self.snake.width/2 or new_head_x <= self.snake.width/2:
+                self.game_over = True
+            elif new_head_y >= 687 - self.snake.height/2 or new_head_y <= self.snake.height/2:
+                self.game_over = True
+                
+                
+            dis = arcade.get_distance(self.food.app_x, self.food.app_y, new_head_x, new_head_y)
             if dis <= 25:
                 self.food = Food()
+            else :
+                if len(self.snake.body) > 1:
+                    self.snake.body.pop()
     
 game = GameView()
 window.show_view(game)
